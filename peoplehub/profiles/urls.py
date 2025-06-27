1b660b9c8d18cb0e5ee9c2ba9cdb87e8986a4521
Tyler Chenoweth <tchen358@gmail.com>
2025-06-27 20:17:05 +0000

    removed the static image storage stuff

--- peoplehub/profiles/urls.py
+++ peoplehub/profiles/urls.py
@@ -5,8 +5,7 @@ from . import views
 from rest_framework.routers import DefaultRouter
 from .views import HobbyViewSet
 
-from django.conf import settings
-from django.conf.urls.static import static
+
 
 router = DefaultRouter()
 router.register('hobbies', HobbyViewSet)
@@ -14,4 +13,5 @@ router.register('hobbies', HobbyViewSet)
 urlpatterns = [
     path("", views.home, name="home"),
     path("api/", include(router.urls))
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] 
+
