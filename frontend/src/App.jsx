7bfcd4509f5685824cfc2bff478290d350effb47
Tyler Chenoweth <tchen358@gmail.com>
2025-06-29 23:39:12 +0000

    added react routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,42 +1,22 @@
-import { useState, useEffect } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import ReactDOM from "react-dom/client";
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 
-import './App.css'
-
-import axios from 'axios';
+import ColleagueList from './pages/ColleagueList/ColleagueList';
+import ProfileDetails from './pages/ProfileDetails/ProfileDetails'; 
 
 function App() {
 
-  const [colleagues, setColleagues] = useState([]);
-
-  useEffect(() => {
-    fetch('http://localhost:8000/profiles/api/coleagues/')  // Express API endpoint
-      .then(res => res.json())
-      .then(data => setColleagues(data)); 
-  }, []);
-
   return (
-    
     <div>
-      {colleagues.map(colleague => (
-        <div className="colleagueRow" key={colleague.id}>
-          <p>
-            {colleague.first_name}
-          </p>
-          <p>&nbsp;</p>
-          <p>
-            {colleague.last_name}
-          </p>
-          
-          
-        </div>
-      ))}
+
+      <Routes>
+        <Route path="/" element={<ColleagueList />} />
+        <Route path="/details" element={<ProfileDetails />} />
+      </Routes>
     </div>
-
-
-    
   )
+  
+
 }
 
 export default App
