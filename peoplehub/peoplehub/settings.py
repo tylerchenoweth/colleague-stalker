81ff963147e5d03c73bdf77b8483bcdece748cd8
Tyler Chenoweth <tchen358@gmail.com>
2025-07-01 03:45:58 +0000

    moved the cors settings to be before another middleware setting

--- peoplehub/peoplehub/settings.py
+++ peoplehub/peoplehub/settings.py
@@ -46,13 +46,16 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+
+    # for connecting to frontend
+    # must go before the CommonMiddleware
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    # for connecting to frontend
-    'corsheaders.middleware.CorsMiddleware',
+    
 ]
 
 ROOT_URLCONF = 'peoplehub.urls'
